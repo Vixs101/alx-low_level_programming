ebdba2b63d13b39c3490f033f03d7fd1a2cdb50e
Vixs101 <elijahvix695@gmail.com>
2023-04-20 19:27:27 +0000

    task 0 correction

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,18 +17,17 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(arg, n);
 
-	if (n != 0)
+	if (n == 0)
+	{
+		return (0);
+	}
 	{
 		for (i = 0; i < n; i++)
 		{
 			sum += va_arg(arg, const unsigned int);
 		}
 	}
-	else
-	{
-		return (0);
-	}
-
+	
 	va_end(arg);
 	return (sum);
 }
