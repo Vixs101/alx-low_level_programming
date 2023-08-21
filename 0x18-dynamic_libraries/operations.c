659025a4a964a8bb92b027a0fb2dbb8ade2c08f8
Vixs101 <elijahvix695@gmail.com>
2023-08-21 16:50:42 +0000

    A c file that will be called by python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - a function that adds two numbers
+ * @a: first variable
+ * @b: second variable
+ * Return: returns the sum of two ints
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - a function that substracts two numbers
+ * @a: first variable
+ * @b: second variable
+ * Return: returns the difference btw two ints
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first variable
+ * @b: second variable
+ * Return: returns the product of two numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: quotient of two numbers
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds the % of two numbers
+ * @a: first
+ * @b: second
+ * Return: the remainder of a division
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
