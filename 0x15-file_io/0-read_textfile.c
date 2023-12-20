593f34fcd73d0a68436a5448c009b8f45d8c86db
Vixs101 <elijahvix695@gmail.com>
2023-12-20 04:40:57 +0100

    fixed a segmentation fault error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fileDescriptor;
-	char buffer[100];
+	char *buffer;
 	ssize_t bytesRead;
 	ssize_t lettersWritten;
 
@@ -21,16 +21,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fileDescriptor == -1)
 		return (0);
 
+	buffer = malloc(letters + 1);
+	if (buffer == NULL)
+	{
+		return (0);
+		close(fileDescriptor);
+	}
+
 	bytesRead = read(fileDescriptor, buffer, letters);
 
 	if (bytesRead == -1)
+	{
+		free(buffer);
 		return (0);
-
+	}
 	lettersWritten = write(STDOUT_FILENO, buffer, bytesRead);
 
 	if (lettersWritten == -1 || lettersWritten != bytesRead)
+	{
+		free(buffer);
 		return (0);
+	}
 
 	close(fileDescriptor);
+	free(buffer);
 	return (lettersWritten);
 }
