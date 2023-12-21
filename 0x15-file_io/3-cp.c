32c84c922ea8743aee32757e4dec3ac68c8b6b16
Vixs101 <elijahvix695@gmail.com>
2023-12-21 16:38:42 +0100

    created a program that copies the content of one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,3 +29,67 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
+
+/**
+ * copyFile - a function that copies the content of one file to another.
+ * @sourceFile: file to be copied from.
+ * @destinationFile: file to be copied to.
+ * Return: 0 on success.
+ */
+
+ssize_t copyFile(const char *sourceFile, const char *destinationFile)
+{
+	int fd1, fd2, closefd1, closefd2;
+	char buffer[1024];
+	ssize_t bytesRead, bytesWritten;
+
+	fd1 = open(sourceFile, O_RDONLY);
+
+	if (fd1 == -1 || access(sourceFile, F_OK) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", sourceFile);
+		return (-1);
+	}
+
+	fd2 = open(destinationFile, O_WRONLY | O_TRUNC | O_CREAT, 0664);
+
+	if (fd2 == -1)
+	{
+		close(fd1);
+		close(fd2);
+		return (-1);
+	}
+
+	while ((bytesRead = read(fd1, buffer, sizeof(buffer) - 1)) > 0)
+	{
+		bytesWritten = write(fd2, buffer, bytesRead);
+
+		if (bytesWritten == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", destinationFile);
+			exit(99);
+		}
+	}
+
+	if (bytesRead == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", sourceFile);
+		close(fd1);
+		close(fd2);
+		exit(98);
+	}
+
+	closefd1 = close(fd1);
+	if (closefd1 == -1)
+	{
+		closefd2 = close(fd2);
+		if (closefd2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+			exit(100);
+		}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
